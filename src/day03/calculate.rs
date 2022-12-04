0706fe52a2af9f85df8efbbf1b40fd5ad54dc1b0
Michael <dr.lodisy@gmail.com>
2022-12-04 11:36:21 +0800

    add day 03

---
+++ src/day03/calculate.rs
@@ -0,0 +1,97 @@
+use std::collections::HashSet;
+
+const ALPHABET: [char; 52] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+
+#[allow(dead_code)]
+pub fn calculate() -> [usize; 2] {
+    // Part one
+    let mut duplicates: Vec<HashSet<char>> = Vec::new();
+
+    include_str!("./input.txt")
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .for_each(|&s| {
+            let str = s.chars().collect::<Vec<char>>();
+
+            let (left, right) = str.split_at(s.len() / 2);
+
+            let mut h: HashSet<char> = HashSet::new();
+
+            for j in 0..left.len() {
+                let p = right.iter().position(|r| *r == left[j]);
+
+                if let Some(position) = p {
+                    h.insert(right[position]);
+                }
+            }
+
+            duplicates.push(h);
+        });
+
+    let mut sum_one: usize = 0;
+
+    duplicates.iter().for_each(|array| {
+        sum_one = sum_one
+            + array
+                .iter()
+                .map(|key| match ALPHABET.iter().position(|c| c == key) {
+                    Some(position) => position + 1,
+                    _ => 0,
+                })
+                .sum::<usize>();
+    });
+
+    // Part two
+    let strings_two: Vec<&str> = include_str!("./input.txt")
+        .split_inclusive("\n")
+        .collect::<Vec<&str>>();
+
+    let mut index: usize = 0;
+
+    let mut result: Vec<String> = Vec::new();
+
+    let mut duplicates_two: Vec<char> = Vec::new();
+
+    while index < strings_two.len() - 2 {
+        result.push(
+            [
+                strings_two[index],
+                strings_two[index + 1],
+                strings_two[index + 2],
+            ]
+            .concat(),
+        );
+
+        index += 3;
+    }
+
+    result.iter().for_each(|s| {
+        let a = s.split("\n").collect::<Vec<&str>>();
+
+        let b = a[0].chars().collect::<Vec<char>>();
+
+        for i in 0..b.len() {
+            if let Some(_) = a[1].find(b[i]) {
+                if let Some(_) = a[2].find(b[i]) {
+                    duplicates_two.push(b[i]);
+                    break;
+                }
+            }
+        }
+    });
+
+    let sum_two = duplicates_two
+        .iter()
+        .map(|key| match ALPHABET.iter().position(|c| c == key) {
+            Some(position) => position + 1,
+            _ => 0,
+        })
+        .sum::<usize>();
+
+    [sum_one, sum_two]
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ mod day02 {
     pub mod rock_paper_scissors;
 }
 
+mod day03 {
+    pub mod calculate;
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -27,5 +31,14 @@ mod tests {
         let result_two = crate::day02::rock_paper_scissors::calculate(false);
 
         assert_eq!(result_two, 13187);
+    }
+
+    #[test]
+    fn rucksack_reorganization() {
+        let [sum_one, sum_two] = crate::day03::calculate::calculate();
+
+        assert_eq!(sum_one, 7889);
+
+        assert_eq!(sum_two, 2825);
     }
 }
